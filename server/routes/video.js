dd3bb84ce258420c4ba272e55a78df23b0fb1633
이상호 <kigkm@naver.com>
2021-02-20 02:13:04 +0900

    first commit

---
+++ server/routes/video.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+var ffmpeg = require('fluent-ffmpeg');
+
+// const { Video } = require('../models/Video');
+// const { Subscriber } = require('../models/Subscriber');
+// const { auth } = require('../middleware/auth');
+
+var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  },
+  fileFilter: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    if (ext !== '.mp4') {
+      return cb(res.status(400).end('mp4 is allowed'), false);
+    }
+    cb(null, true);
+  },
+});
+
+var upload = multer({ storage: storage }).single('file');
+
+//=================================
+//             User
+//=================================
+
+router.post('/uploadfiles', (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      return res.json({ success: false, err });
+    }
+    return res.json({
+      success: true,
+      filePath: res.req.file.path,
+      fileName: res.req.file.filename,
+    });
+  });
+});
+
+router.post('/thumbnail', (req, res) => {
+  let filePath = '';
+  let fileDuration = '';
+
+  ffmpeg.ffprobe(req.body.url, (err, metadata) => {
+    console.dir(metadata);
+    console.log(metadata.format.duration);
+
+    fileDuration = metadata.format.duration;
+  });
+
+  ffmpeg(req.body.url)
+    .on('filenames', (filenames) => {
+      console.log('Will generate ' + filenames.join(', '));
+      filePath = 'uploads/thumbnails/' + filenames[0];
+    })
+    .on('end', function () {
+      console.log('Screenshots taken');
+      return res.json({
+        success: true,
+        url: filePath,
+        fileDuration: fileDuration,
+      });
+    })
+    .screenshots({
+      // Will take screens at 20%, 40%, 60% and 80% of the video
+      count: 3,
+      folder: 'uploads/thumbnails',
+      size: '320x240',
+      // %b input basename ( filename w/o extension )
+      filename: 'thumbnail-%b.png',
+    });
+});
+
+module.exports = router;
